1e3a551ae527bc3234b356a7a2bca64e4936759b
wu rui <wurui@secoo.com>
2017-01-04 10:23:22 +0800

    Initial Commit

---
+++ Slider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Slider
+//
+//  Created by wurui on 17/1/4.
+//  Copyright © 2017年 wurui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Slider/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Slider
+//
+//  Created by wurui on 17/1/4.
+//  Copyright © 2017年 wurui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
