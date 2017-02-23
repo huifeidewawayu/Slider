671b14810e658b0721fd1043514bd9837508db24
wu rui <wurui@secoo.com>
2017-02-23 15:04:11 +0800

    Add New File

---
+++ Slider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Slider
+//
+//  Created by wurui on 17/1/4.
+//  Copyright © 2017年 wurui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Slider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Slider
+//
+//  Created by wurui on 17/1/4.
+//  Copyright © 2017年 wurui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
